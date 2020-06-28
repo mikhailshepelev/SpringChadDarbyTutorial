b24b517dd7ecdccb5d36149bdee5b1636dc4ee67
mikhailshepelev <romanist-mike@yandex.ru>
2020-06-28 13:26:27 +0300

    section 47 is finished 27.06.2020

--- securityDemo/src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
+++ securityDemo/src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -9,14 +9,29 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "com.luv2code.springsecurity.demo")
+@ComponentScan(basePackages="com.luv2code.springsecurity.demo")
 public class DemoAppConfig {
-    //define a bean for ViewResolver
-    @Bean
-    public ViewResolver viewResolver(){
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/view/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
-    }
+
+	// define a bean for ViewResolver
+
+	@Bean
+	public ViewResolver viewResolver() {
+		
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+	
 }
+
+
+
+
+
+
+
+
+
