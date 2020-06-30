dbdd966b1d0c1b1f6b3448c1b5fb8fa52f18c32f
mikhailshepelev <romanist-mike@yandex.ru>
2020-06-30 13:13:47 +0300

    section 51 is finished 30.06.2020

--- securityDemo/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ securityDemo/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -24,7 +24,19 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 			.withUser(users.username("mary").password("test123").roles("MANAGER"))
 			.withUser(users.username("susan").password("test123").roles("ADMIN"));
 	}
-	
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests() // restrict access based on the HttpServletRequest
+		.anyRequest().authenticated() //any request to the app must be authenticated
+		.and()
+				.formLogin() //we are customizing the form login process
+		.loginPage("/showMyLoginPage") //shpw our customer form at the request mapping
+		.loginProcessingUrl("/authenticateTheUser") //login form should POST data to this URL for processing
+		.permitAll() //allow everyone to see login page. No need to be logged in
+		.and().logout().permitAll(); //add logout support
+
+	}
 }
 
 
--- securityDemo/src/main/webapp/WEB-INF/view/home.jsp
+++ securityDemo/src/main/webapp/WEB-INF/view/home.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <html>
 
 <head>
@@ -7,9 +8,15 @@
 <body>
 	<h2>luv2code Company Home Page</h2>
 	<hr>
-	
-	Welcome to the luv2code company home page!
 
+	<p>
+		Welcome to the luv2code company home page!
+	</p>
+
+<%--	Add a logout button--%>
+	<form:form action="${pageContext.request.contextPath}/logout" method="post">
+		<input type="submit" value="Logout"/>
+	</form:form>
 </body>
 
 </html>
